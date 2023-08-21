ae03f7a1ecbde48da72d00006452a7d930e7b8f4
elduke717 <spacematcha@proton.me>
2023-08-21 17:02:54 -0400

    add comments and data update

--- app_tracker_cli.py
+++ app_tracker_cli.py
@@ -1,3 +1,4 @@
+# Import the argparse library for creating a command-line interface
 import argparse
 
 # Sample data (replace this with your actual data)
@@ -5,14 +6,19 @@ data = [
     { 'type': 'Applications', 'value': 22 },
     { 'type': 'E-mail', 'value': 2 },
     { 'type': 'Phone Screens', 'value': 1 },
-    # ... other data items ...
+    { 'type': 'Interviews', 'value': 0 },
+    { 'type': 'Rejections', 'value': 9 },
+    { 'type': 'Acceptances', 'value': 0 },
+    { 'type': 'Offers', 'value': 0 }
 ]
 
+# Define a function to view the application tracker data
 def view_data():
     print("Application Tracker Data:")
     for item in data:
         print(f"{item['type']}: {item['value']}")
 
+# Define a function to update a data value based on type
 def update_value(data_type, new_value):
     for item in data:
         if item['type'] == data_type:
@@ -22,13 +28,19 @@ def update_value(data_type, new_value):
     else:
         print(f"Data type '{data_type}' not found.")
 
+# Define the main function that handles command-line arguments
 def main():
+    # Create an ArgumentParser object for handling command-line arguments
     parser = argparse.ArgumentParser(description="Application Tracker CLI")
+    
+    # Define command-line arguments using the add_argument method
     parser.add_argument("--view", action="store_true", help="View the application tracker data")
     parser.add_argument("--update", nargs=2, help="Update a data value (type new_value)")
 
+    # Parse the command-line arguments
     args = parser.parse_args()
 
+    # Check the parsed arguments and execute the corresponding actions
     if args.view:
         view_data()
     elif args.update:
@@ -37,5 +49,6 @@ def main():
     else:
         print("Use --view to view data or --update to update data.")
 
+# Execute the main function if the script is run as the main module
 if __name__ == "__main__":
     main()
